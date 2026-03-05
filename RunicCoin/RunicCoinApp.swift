55e3d329ae91ea14a6914522abbf9381229d80f3
Marcos Isaias Garcia Martinez <xboxlive2846@gmail.com>
2026-03-04 18:31:34 -0600

    Initial Commit

---
+++ RunicCoin/RunicCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunicCoinApp.swift
+//  RunicCoin
+//
+//  Created by Marcos Garcia on 04/03/26.
+//
+
+import SwiftUI
+
+@main
+struct RunicCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
